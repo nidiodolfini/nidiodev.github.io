60b377c44aaa92ea52e0a850aa6d5da87403e9d1
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-05-21 17:32:14 -0300

    codigo limpo

--- codenation.js
+++ codenation.js
@@ -3,7 +3,6 @@ var requestURL  = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?
 var frase = "";
 let teste = JSON.stringify(requestURL);
 
-
 console.log(teste)
 
 function converter() {
@@ -11,16 +10,12 @@ function converter() {
     fetch('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=51504c327b3f44927c9f12fa7536e4b620c28f08')
     .then(res => res.json())
     .then((out) => {
-        //console.log(out.numero_casas);
-        //console.log(out.token);
-        //console.log(out.cifrado);
-        //console.log(out.cifrado.charCodeAt(0))
+
         let b = [];
         b = out.cifrado;
 
         let a = String.fromCharCode((out.cifrado.charCodeAt(0)-1));
 
-        //console.log(b.length);
        for (var i = 0; i < b.length; i++ ){
            var code = b.charCodeAt(i)
 
@@ -29,16 +24,10 @@ function converter() {
            } else{
             frase+= (String.fromCharCode(code-1))
            }
-           
 
-
-           //console.log( "letra " +  String.fromCharCode(code))
-           //console.log("charcode " + code)
        }
        console.log(frase)
-        
-       /* console.log(a);
-        console.log(out.resumo_criptografico);*/
+
 }).catch(err => console.error(err));
 }
 
