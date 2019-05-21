1a48a110ebe9648e468b3927ebecb9ed0660859a
Nidio Dolfini <nidiosdolfini@gmail.com>
2019-05-21 18:04:13 -0300

    indentado

--- codenation.js
+++ codenation.js
@@ -1,33 +1,34 @@
-var requestURL  = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=51504c327b3f44927c9f12fa7536e4b620c28f08"
+var requestURL = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=51504c327b3f44927c9f12fa7536e4b620c28f08"
 
 var frase = "";
 let teste = JSON.stringify(requestURL);
 
 console.log(teste)
-
+var out;
 function converter() {
-  
+
     fetch('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=51504c327b3f44927c9f12fa7536e4b620c28f08')
-    .then(res => res.json())
-    .then((out) => {
+        .then(res => res.json())
+        .then((out) => {
+            let numero_casas = out.numero_casas;
+            console.log(numero_casas)
+            let b = [];
+            b = out.cifrado;
 
-        let b = [];
-        b = out.cifrado;
+            for (var i = 0; i < b.length; i++) {
+                var code = b.charCodeAt(i)
 
-        let a = String.fromCharCode((out.cifrado.charCodeAt(0)-1));
+                if (code == 32 || code == 33) {
+                    frase += (String.fromCharCode(code))
+                } else {
+                    frase += (String.fromCharCode(code - 1))
+                }
 
-       for (var i = 0; i < b.length; i++ ){
-           var code = b.charCodeAt(i)
+            }
+            //console.log(frase)
+            //console.log(out)
 
-           if(code == 32 || code == 33){
-            frase+= (String.fromCharCode(code))
-           } else{
-            frase+= (String.fromCharCode(code-1))
-           }
-
-       }
-       console.log(frase)
-
-}).catch(err => console.error(err));
+        }).catch(err => console.error(err));
+        
 }
 
